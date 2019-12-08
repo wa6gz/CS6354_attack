33e2c60b419815199689d7458cf1e57a6e18728d
Will <wa6gz@virginia.edu>
2019-12-08 17:19:43 -0500

    more errors

--- covert.c
+++ covert.c
@@ -272,7 +272,7 @@ int main()
             max_set = -22;
         }
         fprintf(out, "%c", 32 + max_set);
-        fprintf(stderr, "%c", 32 + max_set);
+        fprintf(stderr, "*%c*\n", 32 + max_set);
         max_count = max_set = 0;
     }
     fclose(in);
